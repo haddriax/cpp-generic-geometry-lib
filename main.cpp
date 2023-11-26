54a751d44e858e6a72fe08a57cacc3c6601a0df4
Gaël <HadriaxDev@protonmail.com>
2023-11-26 11:22:47 +0100

    Add files to build project.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "source/Vector.h"
+
+int main() {
+
+    Geometry::Vec3f vec1(1.0f, 2.0f, 3.0f);
+    Geometry::Vec2f vec2 = vec1.xy();
+
+    std::cout << "vec3f: (" << vec1.data()[0] << ", " << vec1.data()[1] << ", " << vec1.data()[2] << ')' << std::endl;
+    std::cout << "vec2f: (" << vec2.data()[0] << ", " << vec2.data()[1] << ')' << std::endl;
+
+    return 0;
+}
