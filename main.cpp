54b29b6c79bcf2d843421a612bac1b6b3b39eb16
Gaël <HadriaxDev@protonmail.com>
2023-11-27 15:55:47 +0100

    Fix xyz() method, implement copy, assignement and <<.

--- main.cpp
+++ main.cpp
@@ -4,20 +4,10 @@
 
 int main()
 {
-
-    Geometry::Vec3f vec1(1.0f, 2.0f, 3.0f);
-    Geometry::Vec2f vec2 = vec1.xy();
-    auto vec3 = (vec1 + vec2);
-
-    Geometry::Vector<3, double> v_3_d(3.2, 6, 9.6);
-    Geometry::Vector<2, int> v_2_i(7, 4);
-    auto c_vec = v_3_d - v_2_i;
-    auto c_vec2 = v_2_i + v_3_d;
-    std::cout << "vec3f: (" << c_vec.data()[0] << ", " << c_vec.data()[1] << ", " << c_vec.data()[2] << ')' << std::endl;
-
-    // std::cout << "vec3f: (" << vec1.data()[0] << ", " << vec1.data()[1] << ", " << vec1.data()[2] << ')' << std::endl;
-    // std::cout << "vec2f: (" << vec2.data()[0] << ", " << vec2.data()[1] << ')' << std::endl;
-    // std::cout << "vec3f: (" << vec3.data()[0] << ", " << vec3.data()[1] << ", " << vec3.data()[2] << ')' << std::endl;
+    const Geometry::Vec3f vec1(1.0f, 2.0f, 3.0f);
+    std::cout << vec1.xyz() << std::endl;
+    std::cout << vec1.xy() << std::endl;
+    std::cout << vec1.x() << std::endl;
 
     return 0;
 }
--- source/Vector.h
+++ source/Vector.h
@@ -4,24 +4,44 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <algorithm>
+#include <ostream>
 #include <type_traits>
 #include <utility>
 
 namespace Geometry {
+
+    template<unsigned int Dim, typename T>
+    class Vector;
+
+    template<unsigned int Dim, typename T>
+    std::ostream& operator<< (std::ostream& os, const Vector<Dim, T>& v);
+
     template<unsigned int Dim, typename T>
     class Vector {
     private:
-        T _data[Dim];
+        std::array<T, Dim> _data;
 
     public:
         // Default constructor, initializes all elements to zero.
         Vector();
+
+        // Copy constructor.
+        Vector(const Vector& other) {
+            _data = std::copy(other.data());
+        }
+
+        // Move constructor.
+        Vector(const Vector&& other) noexcept : _data(std::move(other._data)) {
+        }
 
         // N args constructor.
         template<typename... Args>
             requires (sizeof...(Args) == Dim) // C++20
         explicit Vector(Args&&... args) : _data{std::forward<Args>(args)...} {
         }
+
+        virtual ~Vector() = default;
 
         /* ----- Recursive initialization, for C++ < 17 ----
         template<typename... Args>
@@ -43,28 +63,45 @@ namespace Geometry {
         */
 
         // Accessor to get the raw data array.
-        inline const T* data() const {
+        const std::array<T,Dim >& data() const {
             return _data;
         }
 
         // SFINAE (Substitution Failure Is Not An Error)
         template<unsigned int D = Dim>
-        inline std::enable_if_t<D >= 1, Vector<1, T>> x() const;
+        std::enable_if_t<D >= 1, T> x() const;
 
         template<unsigned int D = Dim>
         // inline typename std::enable_if<d >= 2, Vector<2, T>>::type xy() const; // <=> enable_if_t
-        inline std::enable_if_t<D >= 2, Vector<2, T>> xy() const;
+        std::enable_if_t<D >= 2, Vector<2, T>> xy() const;
 
         template<unsigned int D = Dim>
-        inline std::enable_if_t<D >= 3, Vector<2, T>> xyz() const;
+        std::enable_if_t<D >= 3, Vector<3, T>> xyz() const;
 
         // Operators overload
-        inline const T& operator[](std::size_t index) const {
+        /**
+         * \brief Access the vector dimensions.
+         * \param index dim index.
+         * \return vector coordinate.
+         */
+        const T& operator[](std::size_t index) const {
             return _data[index];
         }
 
-        inline T& operator[](std::size_t index) {
+        T& operator[](std::size_t index) {
             return _data[index];
+        }
+
+        // Copy assignement
+        Vector& operator=(const Vector& other) {
+            *this = Vector(other);
+            return (*this);
+        }
+
+        // Move assignement
+        Vector& operator=(Vector&& other) noexcept {
+            *this = Vector(other);
+            return (*this);
         }
 
         /**
@@ -73,16 +110,15 @@ namespace Geometry {
          * \return new vector from (this + other), matching first vector type and dim.
          */
         template<unsigned int Dim2, typename T2>
-        inline auto operator+(const Vector<Dim2, T2>&other) const -> Vector<Dim, T>;
+        auto operator+(const Vector<Dim2, T2>&other) const -> Vector<Dim, T>;
 
         /**
          * \brief - operator, substract two vectors dim by dim.
          * \param other vector to add.
          * \return new vector from (this - other), matching first vector type and dim.
         */
-
         template<unsigned int Dim2, typename T2>
-        inline auto operator-(const Vector<Dim2, T2>&other) const -> Vector<Dim, T>;
+        auto operator-(const Vector<Dim2, T2>&other) const -> Vector<Dim, T>;
 
         /**
          * \brief * operator, multiply two vectors dim by dim.
@@ -90,7 +126,16 @@ namespace Geometry {
          * \return new vector from (this * other), matching first vector type and dim.
          */
         template<unsigned int Dim2, typename T2>
-        inline auto operator*(const Vector<Dim2, T2>&other) const -> Vector<Dim, T>;
+        auto operator*(const Vector<Dim2, T2>&other) const -> Vector<Dim, T>;
+
+        friend std::ostream& operator<<(std::ostream& os, const Vector<Dim, T>& v) {
+            os << "Vector" << Dim << '[';
+            for(auto i = 0; i < v._data.size() - 1; ++i)
+                os << v._data[i] << ';';
+            os << v._data[v._data.size()-1] << ']';
+
+            return os;
+        }
 
         /**
          * \brief Compute the dot product of this . other
@@ -99,7 +144,7 @@ namespace Geometry {
          * \return Dot produt of this with other.
          */
         template<typename T2>
-        inline auto dot(const Vector<Dim, T2>&other);
+        auto dot(const Vector<Dim, T2>&other);
     };
 
     template<unsigned Dim, typename T>
@@ -111,8 +156,8 @@ namespace Geometry {
 
     template<unsigned Dim, typename T>
     template<unsigned D>
-    std::enable_if_t<D >= 1, Vector<1, T>> Vector<Dim, T>::x() const {
-        return Vector<1, T>(_data[0]);
+    std::enable_if_t<D >= 1, T> Vector<Dim, T>::x() const {
+        return _data[0];
     }
 
     template<unsigned Dim, typename T>
@@ -123,7 +168,7 @@ namespace Geometry {
 
     template<unsigned Dim, typename T>
     template<unsigned D>
-    std::enable_if_t<D >= 3, Vector<2, T>> Vector<Dim, T>::xyz() const {
+    std::enable_if_t<D >= 3, Vector<3, T>> Vector<Dim, T>::xyz() const {
         return Vector<3, T>(_data[0], _data[1], _data[2]);
     }
 
